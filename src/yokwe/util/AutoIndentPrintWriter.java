a64b1683ecc0376dced34421fd32c45e081b2896
hasegawa <hasegawa@ubuntudev.local>
2020-02-07 12:49:53 +0900

    Handle double quote and comment properly.

--- src/yokwe/util/AutoIndentPrintWriter.java
+++ src/yokwe/util/AutoIndentPrintWriter.java
@@ -23,26 +23,108 @@ public class AutoIndentPrintWriter implements AutoCloseable {
 	public void println() {
 		out.println();
 	}
+	
+	private String stripString(String string) {
+		StringBuffer ret = new StringBuffer();
+		
+		boolean insideDoubleQuote = false;
+		
+		int length = string.length();
+		for(int i = 0; i < length; i++) {
+			char c1 = string.charAt(i);
+			char c2 = (i == length - 1) ? '\0' : string.charAt(i + 1);
+			
+			// line comment
+			if ((!insideDoubleQuote) && c1 == '/' && c2 == '/') {
+				break;
+			}
+			// ordinary comment
+			if ((!insideDoubleQuote) && c1 == '/' && c2 == '*') {
+				logger.error("Unexpected java comment");
+				logger.error("  string {}!", string);
+				throw new UnexpectedException("level < 0");
+			}
+			
+			if (insideDoubleQuote) {
+				//
+			} else {
+				// line comment
+				if (c1 == '/' && c2 == '/') {
+					break;
+				}
+				// ordinary comment
+				if (c1 == '/' && c2 == '*') {
+					logger.error("Unexpected java comment");
+					logger.error("  string {}!", string);
+					throw new UnexpectedException("level < 0");
+				}
+			}
+			
+			// \" can be appeared inside double quote
+			if (c1 == '\\' && c2 == '"') {
+				i++; // advance i to skip backslash
+				continue;
+			}
+			
+			if (c1 == '"') {
+				insideDoubleQuote = !insideDoubleQuote;
+				continue;
+			}
+			
+			if (!insideDoubleQuote) {
+				switch(c1) {
+				case '{':
+				case '}':
+				case '(':
+				case ')':
+					ret.append(c1);
+					break;
+				default:
+					break;
+				}
+			}
+		}
+		
+		return ret.toString();
+	}
 	public void println(String string) {
+		String strippedString = stripString(string);
+		
+		// adjust level
+		for(char c: strippedString.toCharArray()) {
+			switch (c) {
+			case '}':
+				level--;
+				if (level < 0) {
+					logger.error("level < 0");
+					logger.error("  string {}!", string);
+					logger.error("  strippedString {}!", strippedString);
+					throw new UnexpectedException("level < 0");
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
 		for(int i = 0; i < level; i++) out.print(INDENT);
 		out.println(string);
 		
 		// adjust level
-		for(int i = 0; i < string.length(); i++) {
-			char c = string.charAt(i);
+		for(char c: strippedString.toCharArray()) {
 			switch (c) {
-			case '{':
-			case '(':
-				level++;
-				break;
-			case '}':
 			case ')':
 				level--;
 				if (level < 0) {
 					logger.error("level < 0");
 					logger.error("  string {}!", string);
+					logger.error("  strippedString {}!", strippedString);
 					throw new UnexpectedException("level < 0");
 				}
+				break;
+			case '{':
+			case '(':
+				level++;
 				break;
 			default:
 				break;
