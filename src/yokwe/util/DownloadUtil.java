90c36a296291e72fa9d27bc24d2eaf83d859a504
hasegawa <hasegawa@ubuntudev.local>
2020-03-21 10:08:37 +0900

    Add ContentMode, TextFileTarget, BinaryFileTarget and ByteArrayTarget.

--- src/yokwe/util/DownloadUtil.java
+++ src/yokwe/util/DownloadUtil.java
@@ -2,6 +2,7 @@ package yokwe.util;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -44,8 +45,15 @@ import yokwe.UnexpectedException;
 public class DownloadUtil {
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(DownloadUtil.class);
 
+	public enum ContentMode {
+		BINARY,
+		TEXT,
+		AUTO,
+	}
+	
 	public static interface Target {
 		public String       getURL();
+		public ContentMode  getContentMode();
 		
 		public OutputStream getOutputStream();
 		public Writer       getWriter();
@@ -54,19 +62,44 @@ public class DownloadUtil {
 		public void afterProcess();
 	}
 	
-	public static class FileTarget implements Target {
-		public final String                url;
-		public final Consumer<FileTarget>  action;
-		public final File                  file;
-		public FileTarget(String url, Consumer<FileTarget> action, File file) {
-			this.url    = url;
-			this.action = action;
-			this.file   = file;
+	public static abstract class AbstractTarget implements Target {
+		private final String       url;
+		private final ContentMode  contentMode;
+		
+		protected AbstractTarget(String url, ContentMode contentMode) {
+			this.url         = url;
+			this.contentMode = contentMode;
 		}
 		
 		@Override
 		public String getURL() {
 			return url;
+		}
+		@Override
+		public ContentMode getContentMode() {
+			return contentMode;
+		}
+		@Override
+		public abstract OutputStream getOutputStream();
+		@Override
+		public abstract Writer getWriter();
+		@Override
+		public abstract void beforeProcess();
+		@Override
+		public abstract void afterProcess();
+	}
+	
+	public static abstract class FileTarget extends AbstractTarget {
+		private final Consumer<FileTarget>  action;
+		private final File                  file;
+		public FileTarget(String url, Consumer<FileTarget> action, ContentMode contentMode, File file) {
+			super(url, contentMode);
+			this.action      = action;
+			this.file        = file;
+		}
+		
+		public File getFile() {
+			return file;
 		}
 		
 		@Override
@@ -113,25 +146,35 @@ public class DownloadUtil {
 			this.action.accept(this);
 		}
 	}
-	public static class StringTarget implements Target {
-		public final String                 url;
-		public final Consumer<StringTarget> action;
-		public final StringWriter           stringWriter;
+	public static class BinaryFileTarget extends FileTarget {
+		public BinaryFileTarget(String url, Consumer<FileTarget> action, File file) {
+			super(url, action, ContentMode.BINARY, file);
+		}
+	}
+	public static class TextFileTarget extends FileTarget {
+		public TextFileTarget(String url, Consumer<FileTarget> action, File file) {
+			super(url, action, ContentMode.TEXT, file);
+		}
+	}
+	public static class AutoFileTarget extends FileTarget {
+		public AutoFileTarget(String url, Consumer<FileTarget> action, File file) {
+			super(url, action, ContentMode.AUTO, file);
+		}
+	}
+	public static class StringTarget extends AbstractTarget {
+		private final Consumer<StringTarget> action;
+		private final StringWriter           writer;
 		
 		public StringTarget(String url, Consumer<StringTarget> action) {
-			this.url          = url;
-			this.action       = action;
-			this.stringWriter = new StringWriter();
+			super(url, ContentMode.TEXT);
+			this.action = action;
+			this.writer = new StringWriter();
 		}
 		
 		public String getString() {
-			return stringWriter.toString();
+			return writer.toString();
 		}
 		
-		@Override
-		public String getURL() {
-			return this.url;
-		}
 		@Override
 		public OutputStream getOutputStream() {
 			logger.error("Unexpected");
@@ -139,19 +182,57 @@ public class DownloadUtil {
 		}
 		@Override
 		public Writer getWriter() {
-			return stringWriter;
+			return writer;
 		}
 		@Override
 		public void beforeProcess() {
-			this.stringWriter.getBuffer().setLength(0);
+			// clear buffer before process
+			this.writer.getBuffer().setLength(0);
 		}
 		@Override
 		public void afterProcess() {
 			// No need to close this.os and this.w. They are closed by ResponseHandlerTarget.handleResponse()
 			// call actions
 			this.action.accept(this);
-			// free memory used in stringWriter
-			this.stringWriter.getBuffer().setLength(0);
+			// clear buffer after process
+			this.writer.getBuffer().setLength(0);
+		}
+	}
+	public static class ByteArrayTarget extends AbstractTarget {
+		private final Consumer<ByteArrayTarget> action;
+		private final ByteArrayOutputStream     outputStream;
+		
+		public ByteArrayTarget(String url, Consumer<ByteArrayTarget> action) {
+			super(url, ContentMode.BINARY);
+			this.action       = action;
+			this.outputStream = new ByteArrayOutputStream();
+		}
+		
+		public byte[] getByteArray() {
+			return outputStream.toByteArray();
+		}
+		
+		@Override
+		public OutputStream getOutputStream() {
+			return outputStream;
+		}
+		@Override
+		public Writer getWriter() {
+			logger.error("Unexpected");
+			throw new UnexpectedException("Unexpected");
+		}
+		@Override
+		public void beforeProcess() {
+			// clear buffer before process
+			this.outputStream.reset();
+		}
+		@Override
+		public void afterProcess() {
+			// No need to close this.os and this.w. They are closed by ResponseHandlerTarget.handleResponse()
+			// call actions
+			this.action.accept(this);
+			// clear buffer after process
+			this.outputStream.reset();
 		}
 	}
 	
@@ -228,11 +309,49 @@ public class DownloadUtil {
 	}
 
 	public static class ResponseHandlerTarget implements ResponseHandler<Void> {
+		private static final int BUFFER_SIZE = 64 * 1024;
 		private final Target target;
 
 		public ResponseHandlerTarget(Target target) {
 			this.target = target;
 		}
+		private static void binaryContent(Target target, InputStream is) throws IOException {
+    		target.beforeProcess();
+    		BufferedOutputStream bos = new BufferedOutputStream(target.getOutputStream(), BUFFER_SIZE);
+            try {
+                byte[] buffer = new byte[4096];
+                for(;;) {
+                	int len = is.read(buffer);
+                	if (len == -1) break;
+                	bos.write(buffer, 0, len);
+                }
+            } finally {
+                is.close();
+                bos.close();
+            	// after close os, call afterProcess
+                target.afterProcess();
+            }
+		}
+		private static void textContent(Target target, InputStream is, Charset charset) throws IOException {
+    		Reader r = new InputStreamReader(is, charset);
+    		
+    		target.beforeProcess();
+    		BufferedWriter bw = new BufferedWriter(target.getWriter(), BUFFER_SIZE);
+            try {
+                char[] buffer = new char[4096];
+                for(;;) {
+                	int len = r.read(buffer);
+                	if (len == -1) break;
+                	bw.write(buffer, 0, len);
+                }
+            } finally {
+            	r.close();
+            	bw.close();
+            	// after close w, call afterProcess
+                target.afterProcess();
+            }
+		}
+		
 		@Override
 		public Void handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
 			StatusLine statusLine = response.getStatusLine();
@@ -252,43 +371,30 @@ public class DownloadUtil {
 	                    return null;
 	                }
 
-	            	if (charset == null) {
-	            		target.beforeProcess();
-	            		BufferedOutputStream bos = new BufferedOutputStream(target.getOutputStream(), 64 * 1024);
-		                try {
-		                    byte[] buffer = new byte[4096];
-		                    for(;;) {
-		                    	int len = is.read(buffer);
-		                    	if (len == -1) break;
-		                    	bos.write(buffer, 0, len);
-		                    }
-		                    return null;
-		                } finally {
-		                    is.close();
-		                    bos.close();
-		                	// after close os, call afterProcess
-		                    target.afterProcess();
-		                }
-	            	} else {
-                		Reader r = new InputStreamReader(is, charset);
-                		
-	            		target.beforeProcess();
-                		BufferedWriter bw = new BufferedWriter(target.getWriter(), 64 * 1024);
-
-		                try {
-		                    char[] buffer = new char[4096];
-		                    for(;;) {
-		                    	int len = r.read(buffer);
-		                    	if (len == -1) break;
-		                    	bw.write(buffer, 0, len);
-		                    }
-		                } finally {
-		                	r.close();
-		                	bw.close();
-		                	// after close w, call afterProcess
-		                    target.afterProcess();
-		                }
-	            	}
+	                ContentMode contentMode = target.getContentMode();
+	                switch(contentMode) {
+	                case TEXT:
+	                	if (charset == null) {
+	                		logger.error("Unexpected contentMode");
+	        	        	logger.error("  url         {}", target.getURL());
+	        	        	logger.error("  contentType {}", contentType);
+	        	        	throw new UnexpectedException("Unexpected contentMode");
+	                	}
+	                	textContent(target, is, charset);
+	                	break;
+	                case BINARY:
+	                	binaryContent(target, is);
+	                	break;
+	                case AUTO:
+	                	if (charset == null) {
+		                	binaryContent(target, is);
+	                	} else {
+		                	textContent(target, is, charset);
+	                	}
+	                	break;
+	                default:
+	                	break;
+	                }
 	            	return null;
 	            }
 	        }
